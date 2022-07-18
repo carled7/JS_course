4e3a65781b5adb58d71c1a44c76fbfde82097e5c
carled7 <carlos_ed03@outlook.com>
2022-07-18 09:49:32 -0300

    calculator refactored with constructor function

--- M4L77_constructor/M4L74_calculator_constructor/script.js
+++ M4L77_constructor/M4L74_calculator_constructor/script.js
@@ -4,51 +4,82 @@ function Calculator() {
 
     this.start = () => {
 
-        document.addEventListener('click', (event) => {
-
-            if (event.target.classList.contains('btn-num'))
-                this.input.value += event.target.innerText;
-
-            else if (event.target.classList.contains('btn-clear'))
-                this.input.value = '';
-
-            else if (event.target.classList.contains('btn-erase'))
-                this.input.value = this.input.value.slice(0, -1);
-
-            else if (event.target.classList.contains('btn-eq')) {
-                if (this.validateInput(this.input.value))
-                    this.executeExpression(this.input.value);
-                else
-                    this.input.value = '';
-            }
-        })
+        this.virtualKeysInterface();
+        this.keyboardInterface();
 
     };
 
-    this.validateInput = (input) => {
+    writeDisplay = (btn) => this.input.value += btn.innerText;
 
+    clearDisplay = () => this.input.value = '';
+
+    deleteDisplay = () => this.input.value = this.input.value.slice(0, -1);
+
+    this.virtualKeysInterface = () => {
+        document.addEventListener('click', (event) => {
+
+            if (event.target.classList.contains('btn-num'))
+                writeDisplay(event.target);
+
+            else if (event.target.classList.contains('btn-clear'))
+                clearDisplay();
+
+            else if (event.target.classList.contains('btn-erase'))
+                deleteDisplay();
+
+            else if (event.target.classList.contains('btn-eq'))
+                this.executeExpression();
+
+        })
+    }
+
+    this.keyboardInterface = () => {
+        this.input.addEventListener('keyup', (event) => {
+
+            if (event.keyCode === 13) {
+                this.executeExpression();
+            } else {
+
+                if (!this.validateExpression())
+                    deleteDisplay();
+
+            }
+        })
+    }
+
+    this.validateExpression = () => {
+
+        const expression = this.input.value;
+        
         const buttons = document.querySelectorAll('.btn');
         let validTypes = [];
 
         buttons.forEach(button => {
             validTypes.push(button.innerText);
         });
-
-        for (let i = 0; i < input.length; i++) {
-
-            if (validTypes.indexOf(input[i]) == -1)
+        for (let i = 0; i < expression.length; i++) {
+            if (validTypes.indexOf(expression[i]) == -1)
                 return false;
 
         }
         return true;
     };
 
-    this.executeExpression = (expression) => {
-        try {
-            this.input.value = eval(expression);
-        } catch (error) {
-            alert("Conta inválida");
+    this.executeExpression = () => {
+
+        const expression = this.input.value;
+
+        if (this.validateExpression(expression) && expression != ''){
+            try {
+                this.input.value = eval(expression);                
+            } catch (error) {
+                alert('Expressão Inválida!');
+            }
+
         }
+        else
+            alert('Expressão Inválida!');
+
     };
 
 }
